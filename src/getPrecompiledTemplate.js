c9422148f024db873e85c20b5427ce81fa300866
Nick Balestra <nbalestra@opentable.com>
2017-01-25 13:44:36 +0000

    simpler signature for getPrecompiledTemplate method

--- src/getPrecompiledTemplate.js
+++ src/getPrecompiledTemplate.js
@@ -1,7 +1,7 @@
 const vm = require('vm');
 
-module.exports = (options, templateString) => {
+module.exports = (templateString, key) => {
   const context = {};
   vm.runInNewContext(templateString, context);
-  return context.oc.components[options.key];
+  return context.oc.components[key];
 };
