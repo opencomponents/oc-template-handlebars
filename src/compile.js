9c55af9de1c5a2086a278181a0984df5d27df7e3
Nick Balestra <nbalestra@opentable.com>
2017-03-21 14:52:24 +0000

    err, res callback params

--- src/compile.js
+++ src/compile.js
@@ -1,4 +1,13 @@
+/* eslint-disable no-var */
+
 const handlebars = require('handlebars');
 
-module.exports = (options, callback) =>
-  callback(handlebars.precompile(options.template));
+module.exports = (options, callback) => {
+  var compiled;
+  try {
+    compiled = handlebars.precompile(options.template);
+  } catch (err) {
+    return callback(err);
+  }
+  return callback(null, compiled);
+};
